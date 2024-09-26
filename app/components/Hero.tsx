81e7adbd89632bf0f30fe144a98041c3565e8b89
icodejsx <72747787+icodejsx@users.noreply.github.com>
2024-09-26 07:41:13 +0100

    fixing the hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -7,31 +7,33 @@ const Hero = () => {
     <div id="hero" className=" pt-1">
       <div
         id="hero"
-        className="  md:h-screen bg-cover bg-center bg-no-repeat rounded-b-3xl mt-24"
+        className="  lg:h-screen bg-cover bg-center bg-no-repeat rounded-b-3xl mt-24"
         style={{ backgroundImage: "url('/hero image.svg')" }}
       >
-        <div className="text-white p-10 md:p-20 flex flex-col justify-between h-full">
+        <div className="text-white pt-10 lg:p-16 md:p-10 container flex flex-col md:gap-32 gap-24 justify-between">
           <div>
-            <h1 className="text-7xl md:text-8xl font-bold text-center md:text-left">
+            <h1 className="text-5xl md:text-7xl lg:text-8xl font-bold  text-left">
               Anambra Web3 Conference
             </h1>
-            <p className="text-4xl md:text-3xl text-center md:text-left">
+            <p className="text-2xl md:text-3xl text-left">
               Exploring the decentralized Web
             </p>
           </div>
-          <div className="mt-10 md:mt-24">
-            <h2 className="text-white text-5xl md:text-4xl text-center md:text-left">
-              <span className="font-bold">2nd Nov 2024</span> 9:00 AM
-            </h2>
-            <p className="md:text-lg text-3xl text-center md:text-left">
-              Hollywood event centre, Awka, Anambra
-            </p>
+          <div className="flex flex-col gap-8">
+            <div>
+              <h2 className="text-white text-3xl md:text-4xl text-left">
+                <span className="font-bold">2nd Nov 2024</span> 9:00 AM
+              </h2>
+              <p className="md:text-lg text-2xl text-left">
+                Hollywood event centre, Awka, Anambra
+              </p>
+            </div>
 
-            <div className="mt-10 flex flex-col md:flex-row md:items-center md:justify-between">
-              <div className="flex flex-col md:flex-row justify-center md:justify-start">
+            <div className=" flex flex-col md:flex-row flex-wrap gap-3 pb-10 md:gap-6 md:items-center md:justify-between">
+              <div className="flex flex-col gap-3 md:flex-row justify-center md:justify-start">
                 <Link
                   href=""
-                  className="text-white text-base bg-primary text-center md:items-center py-3 px-7 rounded-lg mb-3 md:mb-0 md:mr-3"
+                  className="text-white text-base bg-primary text-center md:items-center py-3 px-7 rounded-lg "
                 >
                   Register Here
                 </Link>
@@ -42,15 +44,15 @@ const Hero = () => {
                   Apply to Speak
                 </Link>
               </div>
-              <div className="flex flex-col md:flex-row items-center justify-center md:justify-start mt-10 md:mt-0">
-                <div className="flex items-center justify-center md:mr-20">
+              <div className="flex gap-4 md:flex-row flex-col-reverse lg:items-center justify-center lg:justify-start md:justify-between">
+                <div className="flex items-center justify-center ">
                   <p className="mr-3">Powered By</p>
                   <Image
                     src="/ATC_LOGO_White.png"
                     alt=""
-                    width={80}
-                    height={80}
-                    className="w-12 h-12"
+                    width={100}
+                    height={100}
+                    className=""
                   />
                 </div>
                 <div className="flex justify-center mt-5 md:mt-0 md:mr-10">
