2b3c64257700148758b7f15b94cc27e52e917af3
icodejsx <72747787+icodejsx@users.noreply.github.com>
2024-10-02 19:45:39 +0100

    updating

--- app/components/Agenda.tsx
+++ app/components/Agenda.tsx
@@ -166,6 +166,31 @@ const Agenda = () => {
             </p>
           </div>
         </div>
+
+        {/* Section 6 */}
+        <div className="flex flex-col gap-2 md:gap-4 md:flex-row md:py-10 border-b border-gray-300 text-left">
+          <div className="md:w-1/4 flex md:flex-col gap- md:gap-4 flex-col-reverse ">
+            <p className="font-bold text-2xl lg:text-4xl">
+              09:00<span className="ml-2">am</span>
+            </p>
+            <p className="text-primary ">Starts</p>
+          </div>
+          <div className=" md:mt-0 flex flex-col gap-2 md:gap-4">
+            <p className="font-bold text-xl md:text-2xl"> Registration </p>
+            <p className="flex  items-center">
+              <Image
+                src="/location-05.svg"
+                alt=""
+                width={100}
+                height={100}
+                className="w-6 h-6"
+              />
+              <span className="ml-2">
+                Hollywood event centre, Awka, Anambra
+              </span>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
